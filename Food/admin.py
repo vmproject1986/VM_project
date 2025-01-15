5d91da8a5d0ea0c13d38b54160f12ff26761fba5
Mathew <doakmath@gmail.com>
2025-01-14 19:22:47 -0600

    added admin to all models

--- Food/admin.py
+++ Food/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import GroceryList, Recipe, APIResponse
 
-# Register your models here.
+
+# Inline for Grocery Lists
+class GroceryListInline(admin.TabularInline):
+    model = GroceryList
+    extra = 0  # No empty rows
+    can_delete = True
+    show_change_link = True
+
+
+# Inline for Recipes
+class RecipeInline(admin.TabularInline):
+    model = Recipe
+    extra = 0  # No empty rows
+    can_delete = True
+    show_change_link = True
+
+
+# Customize the User Admin
+class CustomUserAdmin(UserAdmin):
+    inlines = [GroceryListInline, RecipeInline]
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return []
+        return super().get_inline_instances(request, obj)
+
+
+# Unregister the default User admin and register the custom one
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+
+@admin.register(GroceryList)
+class GroceryListAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'timestamp')
+    search_fields = ('name', 'user__username')
+    list_filter = ('timestamp',)
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'grocery_list')
+    search_fields = ('name', 'user__username')
+    list_filter = ('grocery_list',)
+
+@admin.register(APIResponse)
+class APIResponseAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('user__username',)
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import Profile
 
-# Register your models here.
+
+# Inline for Profile
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+
+# Customize the User Admin
+class CustomUserAdmin(UserAdmin):
+    inlines = [Profile]
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return []
+        return super().get_inline_instances(request, obj)
+
+
+# Unregister the default User admin and register the custom one
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+
+# Register the Profile model
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'dietary_preferences', 'health_goals')
