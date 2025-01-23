854a3085f6996207a66d807da82754bddf197ace
Mathew <doakmath@gmail.com>
2025-01-23 13:59:15 -0600

    configuring environments

--- VM_project/wsgi.py
+++ VM_project/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VM_project.settings.prod')
 
 
 
