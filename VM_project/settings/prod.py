d1f5c3f921909133396a7cd01ebe77215ee96dcd
Mathew <doakmath@gmail.com>
2025-01-23 14:33:06 -0600

    added logging and env var

--- VM_project/settings/prod.py
+++ VM_project/settings/prod.py
@@ -34,3 +34,18 @@ CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 3600
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_SSL_REDIRECT = True
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
